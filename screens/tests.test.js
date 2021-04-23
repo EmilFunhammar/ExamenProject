ea239ee20ba452ba47057aff3a3a439831b82190
Emil Funhammar <54981432+EmilFunhammar@users.noreply.github.com>
2021-04-23 16:26:55 +0200

    test

---
+++ screens/tests.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import CreateGameComponent from "../screens/createGameScreen";
+import { render, fireEvent } from "@testing-library/react-native"
+ 
+ 
+it("should change text", ()=>{
+   const {getByText, getByPlaceholderText} = render(<CreateGameComponent/>);
+ 
+   const button = getByText("testbutton");
+   const textInput = getByPlaceholderText("emil")
+ 
+   const nyText = "nyText"
+ 
+   fireEvent.changeText(textInput, nyText)
+   //fireEvent.press(button)
+ 
+   const createdItem = getByText(nyText)
+ 
+   expect(createdItem).not.toBeNull()
+ 
+ 
+})
